a952499b4f1055d81df4ff49abf6af29c41e1b1a
Caleb Jones <bionicle.dragon@gmail.com>
2014-11-05 20:22:08 -0500

    Reorganize verlet code

--- racket/verlet.rkt
+++ racket/verlet.rkt
@@ -1,24 +1,48 @@
 #lang racket
 
-(define (except list n)
-  (append (take list n)
-          (drop list (+ n 1))))
-;; Don't think I can use this one easily
-#|
-(define (sets list)
-  (for/list [n (range 2 (+ 1 (length list)))]
-     (take list n)))
-|#
+;; Vec helpers
+(define (vadd v1 v2)
+  (map + v1 v2))
+(define (vneg v)
+  (map - v))
+(define vinv vneg)
+(define (vsub v1 v2)
+  (map - v1 v2))
+(define (vmul v s)
+  (for/list [(c v)] (* c s)))
+(define (vlen2 v)
+  (apply + (map square v)))
+(define (vlen v)
+  (sqrt (vlen2 v)))
+(define (vsum vs)
+  (foldl1 vadd vs))
+(define (foldl1 f list)
+  (foldl f (first list) (rest list)))
 
+;; Other helpers
 (define (nth list n)
   (if (= n 0)
       (first list)
       (nth (rest list)
            (- n 1))))
+(define (square n)
+  (* n n))
+
+;; Output vectors
+(define (output buf)
+  (display (for/list [(particle buf)]
+           (second particle)))
+  (newline))
+
+;; Produce the sets needed for computing gravity
+(define (except list n)
+  (append (take list n)
+          (drop list (+ n 1))))
 (define (sets list)
   (for/list [(n (range (length list)))]
     (cons (nth list n) (except list n))))
 
+;; Define variables for sim
 (define G 6.674e-11)
 (define particles
   (let [(r (make-pseudo-random-generator))
@@ -28,34 +52,9 @@
             (list
              (- (* (random r) 2) 1)
              (- (* (random r) 2) 1))))))
-;;(define particles '((1000 (1 0)) (1 (0 0))))
-#|
-(define particles 
-  (for*/list [(x (range 5))
-              (y (range 5))]
-    (list 1 (list (- (/ x 2.5) 1)
-                  (- (/ y 2.5) 1)))))
-|#
 (define dt 60)
 
-(define (square n)
-  (* n n))
-
-(define (vadd v1 v2)
-  (map + v1 v2))
-(define (vneg v)
-  (map - v))
-(define vinv vneg)
-(define (vsub v1 v2)
-  (map - v1 v2))
-(define (vmul v s)
-  (for/list [(c v)]
-    (* c s)))
-(define (vlen2 v)
-  (apply + (map square v)))
-(define (vlen v)
-  (sqrt (vlen2 v)))
-
+;; Calculate the force of gravity between two particles
 (define (gravity p1 p2)
   (let* [(delta (vsub (second p1)
                       (second p2)))
@@ -64,12 +63,7 @@
          (force (vmul delta magnitude))]
     (vneg force)))
 
-(define (foldl1 f list)
-  (foldl f (first list) (rest list)))
-
-(define (vsum vs)
-  (foldl1 vadd vs))
-
+;; Compute one step of Verlet integration, and return the new state
 (define (step previous current)
   (for/list [(states (sets current))
              (prev previous)]
@@ -82,12 +76,7 @@
     (list mass (vadd (vsub (vmul position 2) (second prev))
                      (vmul accel (square dt)))))))
 
-(define (output buf)
-  (display (for/list [(particle buf)]
-           (second particle)))
-  (newline))
-  
-
+;; Do steps recursively
 (define (do-step prev cur n)
   (output cur)
   (flush-output)
@@ -97,13 +86,11 @@
              (step prev cur)
              (- n 1))))
 
-(define (main)
-  (do-step particles particles 1000))
-
+;; Main
 (display "2d")
 (newline)
 (flush-output)
-(main)
+(do-step particles particles 1000)
 (display "done")
 (newline)
 (flush-output)
