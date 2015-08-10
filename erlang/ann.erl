ad83f8d3c503dbac2271b5dedb2cf07cbe98a9db
Caleb Jones <bionicle.dragon@gmail.com>
2015-08-10 12:14:02 -0400

    Break neural network code
    The tutorial skips over some key details, so just following it results
    in broken code. Still uploading this for historical purposes.

--- erlang/ann.erl
+++ erlang/ann.erl
@@ -1,56 +1,90 @@
 -module(ann).
--export([perceptron/3, sigmoid/1, dot_prod/2, feed_forward/2,
-        replace_input/2, convert_to_list/1, connect/2]).
+-export([perceptron/3, connect/2, run/0]).
 
 %% Learning this from:
 %% https://erlangcentral.org/wiki/index.php?title=Erlang_and_Neural_Networks
 %% You can follow along there too!
 
-sigmoid(X) ->
-    1 / (1 + math:exp(-X)).
-
 dot_prod(A, B) ->
     lists:sum(lists:zipwith(fun(X, Y) -> X*Y end, A, B)).
 
-feed_forward(Weights, Inputs) ->
-    sigmoid(dot_prod(Weights, Inputs)).
+%% vect_sub(A, B) ->
+%%     lists:zipwith(fun(X, Y) -> X-Y end, A, B).
 
-perceptron(Weights, Inputs, Output_PIDs) ->
+%% vect_norm2(A) ->
+%%     dot_prod(A, A).
+
+feed_forward(Sigmoid, Weights, Inputs) ->
+    Sigmoid(dot_prod(Weights, Inputs)).
+
+perceptron(Weights, Inputs, Sensitivities) ->
+    Sigmoid = fun(X) ->
+                      1 / (1 + math:exp(-X))
+              end,
+    
+    Sigmoid_prime = fun(X) ->
+                            math:exp(-X) / (1 + math:exp(-2 * X))
+                    end,
     receive
+        {learn, Backprop} ->
+            Learning_rate = 0.5,
+            
+            % Calculate the correct sensitivies
+            New_sensitivities = add_sensitivity(Sensitivities, Backprop),
+            Output_value = feed_forward(Sigmoid, Weights, convert_to_values(Inputs)),
+            Deriv_value = feed_forward(Sigmoid_prime, Weights, convert_to_values(Inputs)),
+            Sensitivity = calculate_sensitivity(Backprop, Inputs, New_sensitivities,
+                                                Output_value, Deriv_value),
+            io:format("(~w) New Sensitivies: ~w~n", [self(), New_sensitivities]),
+            io:format("(~w) Calculated Sensitivies: ~w~n", [self(), Sensitivity]),
+
+            % Adjust all the weights
+            Weight_adjustment = lists:map(fun(Input) ->
+                                                  Learning_rate * Sensitivity * Input
+                                          end,
+                                          convert_to_values(Inputs)),
+            New_weights = lists:zip_with(fun(W, D) -> W + D end, Weights, Weight_adjustment),
+            io:format("(~w) Adjusted Weights: ~w~n", [self(), Weights]),
+
+            % Propogate sensitivies and associated weights back to the previous layer
+            lists:zip_with(fun(Weight, Input_PID) ->
+                                   Input_PID ! {learn, {self(), Sensitivity * Weight}}
+                           end,
+                           New_weights,
+                           convert_to_keys(Inputs)),
+            
+            perceptron(New_weights, Inputs, New_sensitivities);
         {stimulate, Input} ->
-            % add Input to Inputs to get New_Inputs...
             New_inputs = replace_input(Inputs, Input),
-            
-            % calculate output of perceptron...
-            Output = feed_forward(Weights, convert_to_list(New_inputs)),
-            
-            % stimulate the perceptron my output is connected to
-            if Output_PIDs =/= [] ->
+            Output_value = feed_forward(Sigmoid, Weights, convert_to_values(New_inputs)),
+            if Sensitivities =/= [] ->
                     lists:foreach(fun(Output_PID) ->
-                                          Output_PID ! {stimulate, {self(), Output}}
+                                          Output_PID ! {stimulate, {self(), Output_value}}
                                   end,
-                                  Output_PIDs);
-               Output_PIDs =:= [] ->
-                    io:format("~w outputs: ~w~n", [self(), Output])
+                                  convert_to_keys(Sensitivities));
+               Sensitivities =:= [] ->
+                    io:format("~w outputs: ~w~n", [self(), Output_value])
             end,
-            perceptron(Weights, New_inputs, Output_PIDs);
+            perceptron(Weights, New_inputs, Sensitivities);
         {connect_to_output, Receiver_PID} ->
-            Combined_output = [Receiver_PID | Output_PIDs],
+            New_sensitivies = add_sensitivity(Sensitivities, {Receiver_PID, 1}),
             io:format("~w output connected to ~w: ~w~n",
-                      [self(), Receiver_PID, Combined_output]),
-            perceptron(Weights, Inputs, Combined_output);
+                      [self(), Receiver_PID, New_sensitivies]),
+            perceptron(Weights, Inputs, New_sensitivies);
         {connect_to_input, Sender_PID} ->
             Combined_input = [{Sender_PID, 0.5} | Inputs],
             io:format("~w inputs connected to ~w: ~w~n",
                       [self(), Sender_PID, Combined_input]),
-            perceptron([0.5 | Weights], Combined_input, Output_PIDs);
+            perceptron([0.5 | Weights], Combined_input, Sensitivities);
         {pass, Input_value} ->
-            lists:foreach(fun(Output_PID) ->
+           lists:foreach(fun(Output_PID) ->
                                   io:format("Stimulating ~w with ~w~n",
                                             [Output_PID, Input_value]),
                                   Output_PID ! {stimulate, {self(), Input_value}}
                           end,
-                          Output_PIDs)
+                         convert_to_keys(Sensitivities));
+        {status} ->
+            io:format("~w~n", [Sensitivities])
     end.
 
 connect(Sender_PID, Receiver_PID) ->
@@ -61,6 +95,50 @@ replace_input(Inputs, Input) ->
     {Input_PID, _} = Input,
     lists:keyreplace(Input_PID, 1, Inputs, Input).
 
-convert_to_list(Inputs) ->
-    lists:map(fun({_, Val}) -> Val end, Inputs).
+convert_to_values(Tuple_list) ->
+    lists:map(fun({_, Val}) -> Val end, Tuple_list).
 
+convert_to_keys(Tuple_list) ->
+    lists:map(fun({Key, _}) -> Key end, Tuple_list).
+
+add_sensitivity(Sensitivies, Backprop) when Sensitivies =/= [] ->
+    replace_input(Sensitivies, Backprop);
+add_sensitivity(Sensitivies, _) when Sensitivies =:= [] ->
+    [].
+
+calculate_sensitivity(_, Inputs, Sensitivities, _, _)
+  when Sensitivities =/= [], Inputs =:= [] ->
+    null;
+calculate_sensitivity(Backprop, Inputs, Sensitivities, Output_value, Deriv_value)
+  when Sensitivities =:= [], Inputs =/= [] ->
+    {_, Training_value} = Backprop,
+    (Training_value - Output_value) * Deriv_value;
+calculate_sensitivity(_, Inputs, Sensitivities, _, Deriv_value)
+  when Sensitivities =/= [], Inputs =/= [] ->
+    Deriv_value * lists:sum(convert_to_values(Sensitivities)).
+
+run() ->
+    X1_pid = spawn(ann, perceptron, [[],[],[]]),
+    X2_pid = spawn(ann, perceptron, [[],[],[]]),
+    H1_pid = spawn(ann, perceptron, [[],[],[]]),
+    H2_pid = spawn(ann, perceptron, [[],[],[]]),
+
+    O_pid = spawn(ann, perceptron, [[],[],[]]),
+
+    ann:connect(X1_pid, H1_pid),
+    ann:connect(X1_pid, H2_pid),
+    
+    ann:connect(X2_pid, H1_pid),
+    ann:connect(X2_pid, H2_pid),
+    
+    ann:connect(X1_pid, O_pid),
+    ann:connect(H2_pid, O_pid),
+    
+    X1_pid ! {status},
+    X2_pid ! {status},
+    H1_pid ! {status},
+    H2_pid ! {status},
+    O_pid ! {status},
+
+    X1_pid ! {pass, 1.8},
+    X2_pid ! {pass, 1.3}.
