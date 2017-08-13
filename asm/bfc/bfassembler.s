6c4c96a5c9d0796e71d4bda04c8c4d9e2f1303c8
Caleb Jones <code@calebjones.net>
2017-08-13 02:57:25 +0000

    Get BFA generating a working ELF executable

--- asm/bfc/bfassembler.s
+++ asm/bfc/bfassembler.s
@@ -1,92 +1,54 @@
 .globl _start
 .text
 
-/*
-
-Fixed purpose registers:
-
-fp - Points to the first instruction in the output code, right below
-     the loop stack.
-ip - Points to the first empty instruction in the output code.
-     Grows downwards.
-sp - Points to the current location in the BF loop stack.
-     Grows upwards.
-
-*/
+SYS_read	= 3
+SYS_write	= 4
 
 _start:
-	sub sp, #4096
-	sub fp, sp, #4
-	mov ip, fp
-
 copy_elf_header:
-	mov r0, #0
-	ldr r2, =ehdr
-	mov r3, #88
-copy_header_loop:
-	ldr r1, [r0, r2]
-	str r1, [ip]
-	sub ip, #4
-	add r0, #4
-	cmp r0, r3
-	blt copy_header_loop
+	mov	r0, #1
+	ldr	r1, =ehdr
+	mov	r2, #fullsize
+	mov	r7, #SYS_write
+	svc	#0
 
-copy_body:
-	mov r0, #0
-	ldr r2, =_exit
-	mov r3, #12
-copy_body_loop:
-	ldr r1, [r0, r2]
-	str r1, [ip]
-	sub ip, #4
-	add r0, #4
-	cmp r0, r3
-	blt copy_body_loop
-
-output_slow:
-	mov r1, fp
-	mov r2, #4
-	mov r7, #4
-output_loop:
-	mov r0, #1
-	svc #0
-	sub r1, #4
-	cmp r1, ip
-	bgt output_loop
+	mov	r0, #1
+	ldr	r1, =_exit
+	mov	r2, #exitsize
+	svc	#0
 
 _exit:
-	mov r0, #0
-	mov r7, #1
-	svc #0
+	mov	r0, #0
+	mov	r7, #1
+	svc	#0
 exitsize = . - _exit
 
-	.align 8
-ehdr:	.byte 0x7f, 'E', 'L', 'F', 1, 1, 1, 0
-	.space 8
-	.hword 2	// Executable
-	.hword 0x28	// Targeting ARM
-	.word 1		// Version 1 of ELF
-	.word 0x0001058	// Address of the entry point
-	.word ehdrsize	// Offset to program header table
-	.word 0		// Section header table counts (empty)
-	.word 0
-	.hword ehdrsize	// The size of the ELF header
-	.hword phdrsize	// The size of the program header
-	.hword 1	// The number of entries in the program header
-	.hword 0	// These next 3 say no section table
-	.hword 0
-	.hword 0
+	.align	4
+ehdr:	.byte	0x7f, 'E', 'L', 'F', 1, 1, 1, 0
+	.space	8		// 08
+	.hword	2		// 10 Executable
+	.hword	0x28		// 12 Targeting ARM
+	.word	1		// 14  Version 1 of ELF
+	.word	0x00100054	// 18 Address of the entry point
+	.word	ehdrsize	// 1C Offset to program header table
+	.word	0		// 20 Section header table counts (empty)
+	.word	0		// 24
+	.hword	ehdrsize	// 28 The size of the ELF header
+	.hword	phdrsize	// 2A The size of the program header
+	.hword	1		// 2C The number of entries in the program header
+	.hword	0		// 2E These next 3 say no section table
+	.hword	0		// 30
+	.hword	0		// 32
 ehdrsize = . - ehdr
-phdr:	.word 1		// Program Type "LOAD"
-	.word 0		// No segment offset
-	.word 0x0001000	// Virtual address of the current segment
-	.word 0x0001000	// Physical address of the current segment
-fsize:	.word 100	// Filesize
-msize:	.word 100	// Memsize
-	.word 5		// Flags
-	.word 0x1000	// Align
+phdr:	.word	1		// 34 Program Type "LOAD"
+	.word	0		// 38 No segment offset
+	.word	0x00100000	// 3C Virtual address of the current segment
+	.word	0x00100000	// 40 Physical address of the current segment
+fsize:	.word	0x60		// 44 Filesize (TO BE OVERWRITTEN)
+msize:	.word	0x60		// 48 Memsize  (TO BE OVERWRITTEN)
+	.word	5		// 4C Flags
+	.word	0x00100000	// 50 Align
 phdrsize = . - phdr
-	.align 8
 fullsize = . - ehdr
 fsizeoff = fsize - ehdr
 msizeoff = msize - ehdr
